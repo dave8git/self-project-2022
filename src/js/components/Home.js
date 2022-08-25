0ebb2b26079f3532cb0329763280d9625bd53fc4
unknown <dave8git@gmail.com>
2022-08-26 01:01:15 +0200

    FIX trying different version of adding class invisible

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -10,47 +10,71 @@ export class Home {
     //thisHome.initPlugin();
     const pluginWrapper = document.querySelector('.home-wrapper');
     new Plugin(data, pluginWrapper);
-    thisHome.initElements(); 
+    //thisHome.initElements(); 
+    thisHome.addListeners();
   }
 
-  hideClass(thisElement) {
-    //const thisHome = this;
-    const clickedElement = thisElement;
-    const href = clickedElement.getAttribute('attr');
-    const categoryElements = document.querySelectorAll('.category');
+  // hideClass(thisElement) {
+  //   //const thisHome = this;
+  //   const clickedElement = thisElement;
+  //   const href = clickedElement.getAttribute('attr');
+  //   const categoryElements = document.querySelectorAll('.category');
 
-    categoryElements.forEach(category => {
-      category.parentNode.parentNode.classList.add('non-visible');
-    });
+  //   categoryElements.forEach(category => {
+  //     category.parentNode.parentNode.classList.add('non-visible');
+  //   });
 
-    const allElemCat = document.querySelectorAll('[attr="'+href+'"]'); 
-    //console.log('!!!!!!!!!!!', [class="${href}"]);
+  //   const allElemCat = document.querySelectorAll('[attr="'+href+'"]'); 
 
-    allElemCat.forEach(elem => elem.parentNode.parentNode.classList.remove('non-visible'));
-    console.log('allElemCat', allElemCat);
-   
-    // categoryElements.forEach(category => {
-    //   // if(category.getAttribute('attr') !== href) {
-    //   //   category.parentNode.parentNode.classList.add('non-visible');
-    //   //   //console.log('category.parentNode', category.parentNode.parentNode);
-    //   //   category.parentNode.parentNode.classList.add('visible');
-    //   // }
+  //   allElemCat.forEach(elem => elem.parentNode.parentNode.classList.remove('non-visible'));
+  //   console.log('allElemCat', allElemCat);
+ 
+  //   allElemCat.forEach(elem => console.log(elem));//elem.classList.remove('non-visible'));
+  // }
 
-    // });
-    allElemCat.forEach(elem => console.log(elem));//elem.classList.remove('non-visible'));
-    
-    //const categoryLinks = document.querySelector
+  // initElements() {
+  //   const thisHome = this;
+  //   const categoryElements = document.querySelectorAll('.category');
+  //   console.log(categoryElements);
+  //   categoryElements.forEach(category => category.addEventListener('click', function (event) {
+  //     event.preventDefault();
+  //     thisHome.hideClass(this);
+  //     console.log('thisthisthis', this);
+  //   }));
+  // }
+
+  classInvisible() {
+    const thisHome = this;
+    console.log(thisHome);
+    const clickedElement = thisHome;
+    const clickedElementAttribute = clickedElement.getAttribute('attr');
+    console.log(clickedElementAttribute);
+    event.preventDefault();
+    const elements = document.querySelectorAll('.audioElement');
+    for (let element of elements) {
+      let attributes = element.getAttribute('attr');
+      let attributesArray = attributes.split(',');
+      
+      for (let attribute of attributesArray) {
+        if(clickedElementAttribute !== attribute) {
+          element.classList.add('non-visible');
+        }
+      }
+    }
+
+
+    console.log('elements', elements);
   }
 
-  initElements() {
+  addListeners() {
     const thisHome = this;
     const categoryElements = document.querySelectorAll('.category');
-    console.log(categoryElements);
-    categoryElements.forEach(category => category.addEventListener('click', function (event) {
-      event.preventDefault();
-      thisHome.hideClass(this);
-      console.log('thisthisthis', this);
-    }));
+
+    for (let categoryElement of categoryElements) {
+      categoryElement.addEventListener('click', thisHome.classInvisible);
+    }
+
+
   }
 
   render() {
